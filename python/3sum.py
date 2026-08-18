1d430f323db592283fb305adc549ce8ca18ada59
Aryan Tiwari <140596860+AryanPROOO@users.noreply.github.com>
2026-08-18 17:37:13 +0530

    LeetCode: update python/3sum.py

---
+++ python/3sum.py
@@ -0,0 +1,36 @@
+# ======================================
+# LeetCode Problem: 3sum
+# Language: python
+# Link: https://leetcode.com/problems/3sum/
+# Synced by: LinkCode
+# Date: 8/18/2026, 5:37:12 PM
+# ======================================
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        result = []
+        nums.sort()
+        for i in range(0,len(nums)-2):
+            if i>0 and nums[i] == nums[i - 1]:
+                continue
+            left = i+1
+            right = len(nums)-1
+            while left < right:
+                total = nums[i]+nums[left]+nums[right]
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left +=1
+                else:
+                    right-=1
+        return result
+
+        
+        
