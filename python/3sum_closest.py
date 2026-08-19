3d9d8d45f6a7654401a93949149a6dc7225f7f7a
Aryan Tiwari <140596860+AryanPROOO@users.noreply.github.com>
2026-08-19 17:57:33 +0530

    LeetCode: update python/3sum_closest.py

---
+++ python/3sum_closest.py
@@ -0,0 +1,32 @@
+# ======================================
+# LeetCode Problem: 3sum closest
+# Language: python
+# Link: https://leetcode.com/problems/3sum-closest/
+# Synced by: LinkCode
+# Date: 8/19/2026, 5:57:33 PM
+# ======================================
+
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        result = nums[0]+nums[1]+nums[2]
+        for i in range (len(nums)):
+            left = i+1
+            right = len(nums) - 1
+            while left < right:
+                total = nums[i]+nums[left] + nums[right]
+                if abs(total - target) < abs(result - target):
+                    result = total
+                if total < target:
+                    left += 1
+
+                elif total > target:
+                    right -= 1
+
+                else:
+                    return total
+        return result
+
+
+        
