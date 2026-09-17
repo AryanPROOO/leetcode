a7beafc68ad765996a1060f02dbd82bfdbf90a61
Aryan Tiwari <140596860+AryanPROOO@users.noreply.github.com>
2026-09-17 18:59:51 +0530

    LeetCode: update python/sort_colors.py

--- python/sort_colors.py
+++ python/sort_colors.py
@@ -3,7 +3,7 @@
 # Language: python
 # Link: https://leetcode.com/problems/sort-colors/
 # Synced by: LinkCode
-# Date: 9/17/2026, 1:34:42 PM
+# Date: 9/17/2026, 6:59:49 PM
 # ======================================
 
 
@@ -15,15 +15,12 @@ class Solution(object):
         high = len(nums) - 1
 
         while mid <= high:
-
             if nums[mid] == 0:
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
-
             elif nums[mid] == 1:
                 mid += 1
-
             else:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
