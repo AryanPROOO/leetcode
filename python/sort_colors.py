c803344dbabf10bf2c777aa5dabf946f6d9d7621
Aryan Tiwari <140596860+AryanPROOO@users.noreply.github.com>
2026-09-17 13:34:43 +0530

    LeetCode: update python/sort_colors.py

---
+++ python/sort_colors.py
@@ -0,0 +1,29 @@
+# ======================================
+# LeetCode Problem: sort colors
+# Language: python
+# Link: https://leetcode.com/problems/sort-colors/
+# Synced by: LinkCode
+# Date: 9/17/2026, 1:34:42 PM
+# ======================================
+
+
+class Solution(object):
+    def sortColors(self, nums):
+
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
