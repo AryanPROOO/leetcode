7b8760e550e8cd3ace517f6f61c63ca3199f4e1b
Aryan Tiwari <140596860+AryanPROOO@users.noreply.github.com>
2026-09-24 03:17:39 +0530

    LeetCode: update python/palindrome_partitioning.py

--- python/palindrome_partitioning.py
+++ python/palindrome_partitioning.py
@@ -3,7 +3,7 @@
 # Language: python
 # Link: https://leetcode.com/problems/palindrome-partitioning/
 # Synced by: LinkCode
-# Date: 9/24/2026, 3:17:17 AM
+# Date: 9/24/2026, 3:17:38 AM
 # ======================================
 
 
@@ -15,9 +15,9 @@ class Solution(object):
                 result.append(current[:])
                 return
             for end in range(start, len(s)):
-                subs = s[start:end+1]
-                if subs == subs[::-1]:
-                    current.append(subs)
+                sub = s[start:end+1]
+                if sub == sub[::-1]:
+                    current.append(sub)
                     backtrack(end+1, current)
                     current.pop()
         backtrack(0, [])
